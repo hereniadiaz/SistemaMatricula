87b0e35f2cc523fffd6aa075010520742ed2082b
ServerLab002 <ServerLab002@DESKTOP-TN490BQ>
2024-05-15 09:55:41 -0600

    Agregar archivos de proyecto.

---
+++ SistemaMatricula/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0; Data Source=G:\1 CLASES 2024\PROGRAMACION 12C\II Parcial\Sistema Matricula 2024\Base de Datos\matricula(2002-2003).mdb")
+        Try
+            con.Open()
+            MsgBox("conectado")
+
+            Dim query = "Select * from estudiante"
+            Dim adap As New OleDbDataAdapter(query, con)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox("No se conectó por: " & ex.Message)
+        End Try
+    End Sub
+End Class
